689e5f5822dff8df6e5a9c47a0b51d7f5eaf1e3e
djalde <djalde.gomes@academico.ifpb.edu.br>
2019-12-13 09:14:01 -0300

    changes

--- back-end/php-web/ping/leandro.batista/api/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $qtd = $_GET['qtd'] ?? '';
-  $ip = $_GET['ip'] ?? '';
-  $result = [];
-  $result['response'] = shell_exec("ping -c $qtd $ip");
-  header("Content-type: application/json; charset=UTF-8");
-  header("Access-Control-Allow-Origin: *");
-  echo json_encode($result);
-?>
--- back-end/php-web/post-hello-fetch/roberto.souza/api/hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $name = $_POST['name'] ?? '';
-  $result = [];
-  $result['body'] = "Olá $name";
-
-  header("Content-type: application/json; charset=UTF-8");
-  header("Access-Control-Allow-Origin: *");
-  echo json_encode($result);
-?>
